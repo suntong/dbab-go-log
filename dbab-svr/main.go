6cbe3a6660d03e78073d0469333b593d9693aa40
Tong Sun <suntong@cpan.org>
2019-06-03 21:10:09 -0400

    - [*] add code structure

--- dbab-svr/main.go
+++ dbab-svr/main.go
@@ -1,3 +1,9 @@
+////////////////////////////////////////////////////////////////////////////
+// Program: dbab-svr
+// Purpose: Pixel Server dbab-svr in Go
+// Authors: Tong Sun (c) 2019, All rights reserved
+////////////////////////////////////////////////////////////////////////////
+
 package main
 
 import (
@@ -6,13 +12,22 @@ import (
 	"os"
 )
 
+////////////////////////////////////////////////////////////////////////////
+// Constant and data type/structure definitions
+
 const (
 	pixel = "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xFF\xFF\xFF\x00\x00\x00\x21\xF9\x04\x01\x00\x00\x00\x00\x2C\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3B"
 )
 
+////////////////////////////////////////////////////////////////////////////
+// Global variables definitions
+
 var (
 	httpPort = ":80" // default is :80
 )
+
+////////////////////////////////////////////////////////////////////////////
+// Function definitions
 
 func pixelserv(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("ETag", "dbab")
@@ -21,6 +36,9 @@ func pixelserv(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "public, max-age=31536000")
 	w.Write([]byte(pixel))
 }
+
+//==========================================================================
+// Main
 
 func main() {
 	_httpPort := os.Getenv("DBAB_SERVER_ADDR")
