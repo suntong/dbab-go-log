0cbd77b7d6b61f349d591eae6dc2f610773008e0
Tong Sun <suntong@cpan.org>
2019-06-03 21:05:03 -0400

    - [+] implemented pixelserv

--- dbab-svr/main.go
+++ dbab-svr/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
+const (
+	pixel = "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xFF\xFF\xFF\x00\x00\x00\x21\xF9\x04\x01\x00\x00\x00\x00\x2C\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3B"
+)
+
+var (
+	httpPort = ":80" // default is :80
+)
+
+func pixelserv(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("ETag", "dbab")
+	w.Header().Set("Connection", "close")
+	w.Header().Set("Content-Type", "image/gif")
+	w.Header().Set("Cache-Control", "public, max-age=31536000")
+	w.Write([]byte(pixel))
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	_httpPort := os.Getenv("DBAB_SERVER_ADDR")
+	if _httpPort != "" {
+		httpPort = _httpPort
+	}
+
+	http.HandleFunc("/", pixelserv)
+	log.Printf("Starting dbab pixel server on port %s\n", httpPort)
+	// Run the web server.
+	log.Fatal(http.ListenAndServe(httpPort, nil))
 }
