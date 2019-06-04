9b934c6141dbc8f836a2e637389094c5275a0aba
Tong Sun <suntong@cpan.org>
2019-06-03 22:20:07 -0400

    - [+] add autoProxy

--- dbab-svr/main.go
+++ dbab-svr/main.go
@@ -1,35 +1,46 @@
 ////////////////////////////////////////////////////////////////////////////
 // Program: dbab-svr
-// Purpose: Pixel Server dbab-svr in Go
+// Purpose: Pixel Server in Go
 // Authors: Tong Sun (c) 2019, All rights reserved
 ////////////////////////////////////////////////////////////////////////////
 
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 ////////////////////////////////////////////////////////////////////////////
 // Constant and data type/structure definitions
 
 const (
+	confFile  = "/etc/dbab/dbab.addr"
+	proxyFile = "/etc/dbab/dbab.proxy"
+
 	pixel = "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xFF\xFF\xFF\x00\x00\x00\x21\xF9\x04\x01\x00\x00\x00\x00\x2C\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3B"
 )
+
+// type proxyHandler struct {
+// 	setting string
+// }
 
 ////////////////////////////////////////////////////////////////////////////
 // Global variables definitions
 
 var (
-	httpPort = ":80" // default is :80
+	progname     = "dbab-svr"
+	proxySetting = ""
 )
 
 ////////////////////////////////////////////////////////////////////////////
 // Function definitions
 
-func pixelserv(w http.ResponseWriter, r *http.Request) {
+func pixelServ(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("ETag", "dbab")
 	w.Header().Set("Connection", "close")
 	w.Header().Set("Content-Type", "image/gif")
@@ -37,16 +48,51 @@ func pixelserv(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(pixel))
 }
 
+//func (h *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func autoProxy(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Connection", "close")
+	w.Header().Set("Content-Type", "application/octet-stream")
+	//w.Write([]byte(h.setting))
+	w.Write([]byte(proxySetting))
+}
+
+//==========================================================================
+// support functions
+
+// readFile returns the single-line file content (less trailing \n) of the file by the given fname
+func readFile(fname string) string {
+	b, err := ioutil.ReadFile(fname)
+	abortOn("Reading input file: "+fname, err)
+	return strings.TrimSuffix(string(b), "\n")
+}
+
+// abortOn will quit on anticipated errors gracefully without stack trace
+func abortOn(errCase string, e error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "[%s] %s error: %v\n", progname, errCase, e)
+		os.Exit(1)
+	}
+}
+
 //==========================================================================
 // Main
 
 func main() {
+	httpPort := readFile(confFile)
 	_httpPort := os.Getenv("DBAB_SERVER_ADDR")
 	if _httpPort != "" {
 		httpPort = _httpPort
-	}
 
-	http.HandleFunc("/", pixelserv)
+	}
+	//autoProxy := &proxyHandler{
+	proxySetting = fmt.Sprintf(
+		"function FindProxyForURL(url, host) { return \"PROXY %s:3128; DIRECT\"; }",
+		readFile(proxyFile))
+
+	http.HandleFunc("/", pixelServ)
+	http.HandleFunc("/proxy.pac", autoProxy)
+	http.HandleFunc("/wpad.dat", autoProxy)
+
 	log.Printf("Starting dbab pixel server on port %s\n", httpPort)
 	// Run the web server.
 	log.Fatal(http.ListenAndServe(httpPort, nil))
